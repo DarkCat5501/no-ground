4d38176ee89f068f5232da14e3872165d3329fff
DarkCat5501 <viccente512@gmail.com>
2024-03-03 22:58:18 -0300

    WIP: added license from wasm-printf source

--- engine/wasmstd.h
+++ engine/wasmstd.h
@@ -17,6 +17,22 @@
 #define WASM_IMPORT_AS(N,R,F,...) extern __attribute__((import_module("env"),import_name(#N))) R F(__VA_ARGS__) 
 #endif
 
+/* printf ported to wasm eumerics/wasm-printf
+
+Copyright (c) 2021 Eumerics Inc.
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+
 WASM_IMPORT(int,printf_string,char const* begin, char const* end);
 WASM_IMPORT(int,printf_integer,int flags, int width, int precision, char length_char, uint8_t length, char type, int value);
 WASM_IMPORT(int,printf_int32,int flags, int width, int precision, char length_char, uint8_t length, char type, int32_t value);
