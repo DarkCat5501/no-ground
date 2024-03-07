86ff97c77dab272ff1400c357f4c4b5ba1b614b1
DarkCat5501 <viccente512@gmail.com>
2024-03-07 14:44:21 -0300

    WIP:added simple client test

--- platforms/web/scripts/client.js
+++ platforms/web/scripts/client.js
@@ -2,6 +2,23 @@
 var conn = { sk: null };
 globalThis.conn = conn;
 
+const id = Math.random();
+var counter = 0;
 export function init() {
   console.log("Init for client");
+  // Create WebSocket connection.
+  const socket = new WebSocket("ws://localhost:8090");
+  // Connection opened
+  socket.addEventListener("open", (event) => {
+    socket.send("Hello world")
+    // setInterval(() => {
+    //   console.log("sent hello world")
+    // }, 100);
+  });
+
+  // Listen for messages
+  socket.addEventListener("message", (event) => {
+    console.log("Message from server ", event.data);
+  });
+  conn.sk = socket;
 }
